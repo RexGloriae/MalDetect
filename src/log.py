d8252b951f94e474aed6e7636de5a50cca4d5a12
Kessel <andreidanielcodreanu@gmail.com>
2025-07-11 21:15:02 +0300

    add rotating logs

--- src/log.py
+++ src/log.py
@@ -1,4 +1,8 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+os.makedirs('logs', exist_ok=True)
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -7,7 +11,11 @@ console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 console.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 
-file = logging.FileHandler('mal.log')
+file = RotatingFileHandler(
+    'logs/entry.log',
+    maxBytes=5*1024*1024,
+    backupCount=3
+)
 file.setLevel(logging.DEBUG)
 file.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
 
