8004e0b3d6aad883d08c37f4f9da5313e8948f7c
Kessel <andreidanielcodreanu@gmail.com>
2025-07-11 21:19:04 +0300

    switch logger to timed one
    log files are rotated every hour, kept for 24h

--- src/log.py
+++ src/log.py
@@ -1,5 +1,5 @@
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 import os
 
 os.makedirs('logs', exist_ok=True)
@@ -11,9 +11,10 @@ console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 console.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 
-file = RotatingFileHandler(
+file = TimedRotatingFileHandler(
     'logs/entry.log',
-    maxBytes=5*1024*1024,
+    when='H',
+    interval=1,
     backupCount=3
 )
 file.setLevel(logging.DEBUG)
